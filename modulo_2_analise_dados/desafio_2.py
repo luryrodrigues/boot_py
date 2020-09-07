589d79952205835d83eb3fb2900b393efce5c5b4
luryrodrigues <lurylrodrigues@gmail.com>
2020-09-07 18:48:56 -0300

    finishing desafio2

--- modulo_2_analise_dados/desafio_2.py
+++ modulo_2_analise_dados/desafio_2.py
@@ -4,55 +4,72 @@ import matplotlib.pyplot as plt
 
 df=pd.read_csv('https://pycourse.s3.amazonaws.com/bike-sharing.csv')
 
-'''print(df.head())
+print(df.head())
 
-print('Qual o tamanho desse dataset?')
+print('a) Qual o tamanho desse dataset?')
 print(df.info())
 
-print('Qual a média da coluna windspeed?')
+print('b) Qual a média da coluna windspeed?')
 print(df['windspeed'].describe())
 
-print('Qual a média da coluna temp?')
+print('c) Qual a média da coluna temp?')
 print(df['temp'].mean())
 
-print('Quantos registros existem para o ano de 2011? E 2012?')
+print('d) Quantos registros existem para o ano de 2011? E 2012?')
 print(df['year'].value_counts())  #0:2011 1:2012
 
-print('Quantas locações de bicicletas foram efetuadas em 2011?')
+print('e) Quantas locações de bicicletas foram efetuadas em 2011?')
 y=df[df['year']==0]
 print(np.sum(y['total_count']))
 
-print('Quantas locações de bicicletas foram efetuadas em 2012?')
+print('f) Quantas locações de bicicletas foram efetuadas em 2012?')
 y=df[df['year']==1]
 print(np.sum(y['total_count']))
 
-print('Qual estação do ano contém a maior média de locações de bicicletas?')
+print('g) Qual estação do ano contém a maior média de locações de bicicletas?')
 lista=[]
 i=1
 for i in range (1,5):
   y=df[df['season']==i]
-  x=np.sum(y['total_count'])
+  x=np.mean(y['total_count'])
   lista.append(x)
   i+=1
-print(lista)
-print(max(lista))'''
+print(lista.index(max(lista)))
 
-print('Qual estação do ano contém a menor média de locações de bicicletas?')
+print('h) Qual estação do ano contém a menor média de locações de bicicletas?')
+print(lista.index(min(lista)))
 
+print('i) Qual horário do dia contém a maior média de locações de bicicletas?')
+lista=[]
+i=0
+for i in range (0,24):
+  y=df[df['hour']==i]
+  x=np.mean(y['total_count'])
+  lista.append(x)
+  i+=1
+print(lista.index(max(lista)))
+print('j) Qual horário do dia contém a menor média de locações de bicicletas?')
+print(lista.index(min(lista)))
 
-'''print('Qual estação do ano contém a menor média de locações de bicicletas?')
+print('k) Que dia da semana contém a maior média de locações de bicicletas?')
+lista=[]
+i=0
+for i in range (0,7):
+  y=df[df['weekday']==i]
+  x=np.mean(y['total_count'])
+  lista.append(x)
+  i+=1
+print(lista.index(max(lista)))
+print('l) Que dia da semana contém a menor média de locações de bicicletas?')
+print(lista.index(min(lista)))
 
+print('m) Às quartas-feiras (weekday = 3), qual o horário do dia contém a maior média de locações de bicicletas?')
+y=df[df['weekday']==3]
+x=y.groupby(by='hour')
+z=(x['total_count']).mean().sort_values(ascending=False)
+print(z.head(1))
 
-print('Qual horário do dia contém a menor média de locações de bicicletas?')
-
-
-print('Que dia da semana contém a maior média de locações de bicicletas?')
-
-
-print('Que dia da semana contém a menor média de locações de bicicletas?')
-
-
-print('Às quartas-feiras (weekday = 3), qual o horário do dia contém a maior média de locações de bicicletas?')
-
-
-print('Aos sábados (weekday = 6), qual o horário do dia contém a maior média de locações de bicicletas?')'''
+print('n) Aos sábados (weekday = 6), qual o horário do dia contém a maior média de locações de bicicletas?')
+y=df[df['weekday']==6]
+x=y.groupby(by='hour')['total_count'].mean().sort_values(ascending=False)
+print(x.head(1))
